f04afcd62167d1c2a661e0e92bcc0650fb8c7672
taitran <taitran12310@gmail.com>
2022-12-19 18:15:17 +0700

    change evaluation output

--- universal_landmark_detection/evaluation.py
+++ universal_landmark_detection/evaluation.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     dic = {}
     pixel_dic = {}
     if not args.output:
-        output = os.path.join(args.input, 'eval')
+        output = os.path.join(args.output, 'eval')
     for d in os.listdir(args.input):
         inp = os.path.join(args.input, d)
         if os.path.isdir(inp):
